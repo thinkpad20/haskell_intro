14143e2d74548fd836f66ce9d85f148291f0aafc
Allen Nelson <ithinkican@gmail.com>
2013-09-23 11:15:17 -0500

    changed to polymorphic vertex

--- meetup 9-21-13/graphSearch.hs
+++ meetup 9-21-13/graphSearch.hs
@@ -1,6 +1,7 @@
-newtype Vertex = Vertex (String, [Vertex]) deriving (Eq)
+data Vertex a = Vertex (a, [Vertex a])
+--data Graph a = 
 
-a, b, c, d, e, f, g :: Vertex
+a, b, c, d, e, f, g :: Vertex String
 a = Vertex ("a", [b,c])
 b = Vertex ("b", [d,e,f]) 
 c = Vertex ("c", [g] )
@@ -9,9 +10,10 @@ e = Vertex ("e", [])
 f = Vertex ("f", []) 
 g = Vertex ("g", []) 
 
-instance Show Vertex where
-  show (Vertex (name, [])) = show name
-  show (Vertex (name, vs)) = "(" ++ show name ++ "->" ++ show vs ++ ")"
+display :: Show a => Vertex a -> String
+display (Vertex (a, [])) = show a
+display (Vertex (a, vs)) = "(" ++ show a ++ "->" ++ display' vs ++ ")" where
+  display' = concat . map display
 
 {- The above nodes define a graph like this:
 
@@ -23,16 +25,16 @@ instance Show Vertex where
 
 -}
 
-visit :: Vertex -> IO ()
-visit v = putStrLn $ "\tvisited " ++ show v
+visit :: Show a => Vertex a -> IO ()
+visit v = putStrLn $ "\tvisited " ++ display v
 
-bfs :: (Vertex -> IO ()) -> Vertex -> IO ()
+bfs :: (Vertex a -> IO ()) -> Vertex a -> IO ()
 bfs fun v = loop fun v [] where
   loop f v@(Vertex (name, [])) [] = f v
   loop f v@(Vertex (name, (a:as))) [] = f v >> loop f a as
   loop f v@(Vertex (name, adj)) (a:as) = f v >> loop f a (as ++ adj)
 
-dfs :: (Vertex -> IO ()) -> Vertex -> IO ()
+dfs :: (Vertex a -> IO ()) -> Vertex a -> IO ()
 dfs f v@(Vertex (_, adj)) = f v >> loop adj where
   loop [] = return ()
   loop (a:as) = dfs f a >> loop as
